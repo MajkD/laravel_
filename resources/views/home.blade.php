337b324a71eb0fdc0c7c31517bcb8b4d6521b836
Mikael Danielsson <mikael.danielsson@zalando.de>
2018-03-05 08:08:48 +0100

    added auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,25 @@
-HOME
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+            {!! link_to_route('skills.index', 'View Skills', [], ['type'=>'button', 'class'=>'btn btn-primary btn-sm']) !!}
+            {!! link_to_route('logout', 'Logout', [], ['type'=>'button', 'class'=>'btn btn-danger btn-sm']) !!}
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('logout', 'Auth\LoginController@logout');
+
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('skills', 'SkillsController@index')->name('skills.index');
